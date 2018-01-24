012b5ab0fae57d9928e046f16b4b02afab39422e
Justin-Dev <studentmedia@imac-3.uwyo.edu>
2018-01-24 16:19:16 -0700

    Navigation/webview
    Got the navigation slider fixed to look better. Have the web view working on the branding iron website, but won't do the other sites.

--- app/src/main/java/uwyostudentmedia/intership/MainActivity.java
+++ app/src/main/java/uwyostudentmedia/intership/MainActivity.java
@@ -12,9 +12,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +27,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +37,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
     }
 
     @Override
@@ -80,18 +80,25 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
+
+        } else if (id == R.id.nav_branding) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            myWebView.stopLoading();
+            myWebView.loadUrl("about:blank");
+            myWebView.loadUrl("http://www.uwbrandingiron.com");
+            //myWebView.reload();
+        } else if (id == R.id.nav_design) {
+            myWebView.stopLoading();
+            myWebView.loadUrl("about:blank");
+            myWebView.loadUrl("http://www.uwdynamicdesign.com");
+            //myWebView.reload();
+        } else if (id == R.id.nav_media) {
+            myWebView.loadUrl("about:blank");
+            myWebView.loadUrl("http://www.uwdynamicmedia.com");
+        } else if (id == R.id.nav_laramie) {
+            myWebView.loadUrl("about:blank");
+            myWebView.loadUrl("http://www.laramieliving.com");
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -99,3 +106,10 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 }
+/*
+private class MyWebViewClient extends WebViewClient {
+
+    @Override
+    public boolean should
+}
+*/
